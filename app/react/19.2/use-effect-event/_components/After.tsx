d487a3be48ff8040894ec0fe026ec1f9b82ea438
R.Moroizumi <m.ryousuke0401@iclioud.com>
2025-11-02 16:50:15 +0900

    refactor: simplify keydown event handling and update button interactions in After and Before components

--- app/react/19.2/use-effect-event/_components/After.tsx
+++ app/react/19.2/use-effect-event/_components/After.tsx
@@ -5,25 +5,53 @@ import { Card } from '@/components/ui/card';
 import { useEffect, useEffectEvent, useState } from 'react';
 import { toast } from 'sonner';
 
-export default function UseEffectEventAfter() {
-  const [count, setCount] = useState(0);
-
-  const cointEventHandler = useEffectEvent(() => {
-    toast.success(`現在のカウント数は ${count} です`);
+const useKeyDown = (
+  targetKey: string,
+  handler: () => void,
+  isEnabled = true,
+) => {
+  // useEffectEventを使用すると、依存関係を追加する必要がない
+  const keydownListener = useEffectEvent((e: KeyboardEvent) => {
+    if (e.key === targetKey) {
+      handler();
+    }
   });
 
   useEffect(() => {
-    window.addEventListener('click', cointEventHandler);
-    return () => window.removeEventListener('click', cointEventHandler);
-  }, []); // ← ここにcointEventHandlerの依存関係を追加する必要がない
+    if (!isEnabled) {
+      return;
+    }
+    document.addEventListener('keydown', keydownListener);
+    return () => {
+      document.removeEventListener('keydown', keydownListener);
+    };
+  }, [isEnabled]); // 依存関係を追加する必要がない
+};
+
+export default function UseEffectEventAfter() {
+  const [key, setKey] = useState('a');
+
+  useKeyDown(key, () => {
+    toast.success(`${key}ボタンが押されました`);
+  });
+
+  const onClick = (key: string) => {
+    setKey(key);
+    toast.message(`${key}ボタンが設定されました`);
+  };
 
   return (
     <Card className="bg-white dark:bg-black p-8">
       <h2 className="text-2xl font-bold font-mono border-b border-gray-200 dark:border-gray-800 pb-2">
         19.2以降
       </h2>
-      <p>カウント数: {count}</p>
-      <Button onClick={() => setCount(count + 1)}>＋ボタン</Button>
+      <p className="text-gray-500 dark:text-gray-400 mb-4 font-bold font-mono">
+        key: {key}
+      </p>
+      <div className="grid grid-cols-2 gap-4">
+        <Button onClick={() => onClick('c')}>cボタン</Button>
+        <Button onClick={() => onClick('d')}>dボタン</Button>
+      </div>
     </Card>
   );
 }
--- app/react/19.2/use-effect-event/_components/Befor.tsx
+++ app/react/19.2/use-effect-event/_components/Befor.tsx
@@ -5,23 +5,51 @@ import { Card } from '@/components/ui/card';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
-export default function UseEffectEventBefor() {
-  const [count, setCount] = useState(0);
-
+const useKeyDown = (
+  targetKey: string,
+  handler: () => void,
+  isEnabled = true,
+) => {
   useEffect(() => {
-    const handler = () => {
-      toast.success(`現在のカウント数は ${count} です`);
+    if (!isEnabled) {
+      return;
+    }
+    const keydownListener = (e: KeyboardEvent) => {
+      if (e.key === targetKey) {
+        handler();
+      }
     };
-    window.addEventListener('click', handler);
-    return () => window.removeEventListener('click', handler);
-  }, [count]); // ← ここに依存関係を追加
+    document.addEventListener('keydown', keydownListener);
+    return () => {
+      document.removeEventListener('keydown', keydownListener);
+    };
+  }, [isEnabled, targetKey, handler]); // 依存関係を追加する必要がある
+};
+
+export default function UseEffectEventBefor() {
+  const [key, setKey] = useState('a');
+
+  useKeyDown(key, () => {
+    toast.success(`${key}ボタンが押されました`);
+  });
+
+  const onClick = (key: string) => {
+    setKey(key);
+    toast.message(`${key}ボタンが設定されました`);
+  };
+
   return (
     <Card className="bg-white dark:bg-black p-8">
       <h2 className="text-2xl font-bold font-mono border-b border-gray-200 dark:border-gray-800 pb-2">
         従来
       </h2>
-      <p>カウント数: {count}</p>
-      <Button onClick={() => setCount(count + 1)}>＋ボタン</Button>
+      <p className="text-gray-500 dark:text-gray-400 mb-4 font-bold font-mono">
+        key: {key}
+      </p>
+      <div className="grid grid-cols-2 gap-4">
+        <Button onClick={() => onClick('a')}>aボタン</Button>
+        <Button onClick={() => onClick('b')}>bボタン</Button>
+      </div>
     </Card>
   );
 }
